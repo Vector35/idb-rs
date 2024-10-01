f433dd2ab194e26c25fac16932c5ad93a9f6f4c6
Rubens Brandao <git@rubens.io>
2024-10-01 13:36:17 -0300

    unify the read id0 into a function

--- src/tools/dump_functions.rs
+++ src/tools/dump_functions.rs
@@ -1,24 +1,12 @@
-use std::fs::File;
-use std::io::BufReader;
+use crate::{get_id0_section, Args};
 
-use crate::{Args, FileType};
-
-use anyhow::{anyhow, Result};
-use idb_rs::{id0::EntryPoint, IDBParser};
+use anyhow::Result;
+use idb_rs::id0::EntryPoint;
 
 pub fn dump_functions(args: &Args) -> Result<()> {
     // parse the id0 sector/file
-    let id0 = match args.input_type() {
-        FileType::Til => return Err(anyhow!("TIL don't contains any ID0 data")),
-        FileType::Idb => {
-            let input = BufReader::new(File::open(&args.input)?);
-            let mut parser = IDBParser::new(input)?;
-            let id0_offset = parser
-                .id0_section_offset()
-                .ok_or_else(|| anyhow!("IDB file don't contains a TIL sector"))?;
-            parser.read_id0_section(id0_offset)?
-        }
-    };
+    let id0 = get_id0_section(args)?;
+
     println!("Function and Comments AKA `$ funcs`: ");
     for entry in id0.functions_and_comments()? {
         match entry? {
--- src/tools/split_idb.rs
+++ src/tools/split_idb.rs
@@ -1,19 +1,14 @@
-use std::{fs::File, io::BufReader};
+use std::fs::File;
+use std::io::BufReader;
 
-use crate::{Args, FileType, SplitIDBArgs};
+use crate::{Args, SplitIDBArgs};
 
-use anyhow::{anyhow, Result};
-use idb_rs::IDBParser;
+use anyhow::Result;
 
 pub fn split_idb(args: &Args, id0_args: &SplitIDBArgs) -> Result<()> {
     // parse the id0 sector/file
-    let parser = match args.input_type() {
-        FileType::IDB => {
-            let input = BufReader::new(File::open(&args.input)?);
-            IDBParser::new(input)?
-        }
-        FileType::TIL => return Err(anyhow!("TIL don't contains any Sections")),
-    };
+    let id0 = get_id0_section(args)?;
+
     // TODO implement uncompressed raw-section read capability
     todo!();
 }
