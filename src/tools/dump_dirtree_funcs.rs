2455dc4276fcdc1b6be36073ade7f3178105bc34
Rubens Brandao <git@rubens.io>
2024-10-01 14:23:30 -0300

    include dirtree funcs to the dump-functions dump tool

--- src/tools/dump_dirtree_funcs.rs
+++ src/tools/dump_dirtree_funcs.rs
@@ -15,39 +15,9 @@ pub fn dump_dirtree_funcs(args: &Args) -> Result<()> {
 
 fn print_folder(id0: &ID0Section, identation: usize, dirs: &[DirTreeEntry<u64>]) -> Result<()> {
     for dir in dirs {
-        print_ident(identation);
+        print_indent(identation);
         match dir {
-            DirTreeEntry::Leaf(fun_addr) => {
-                let infos = id0.address_info_at(*fun_addr)?;
-                let mut name = None;
-                let mut ty = None;
-                for info in infos {
-                    match info? {
-                        idb_rs::id0::AddressInfo::Comment(_)
-                        | idb_rs::id0::AddressInfo::Other { .. } => {}
-                        idb_rs::id0::AddressInfo::Label(label) => {
-                            if let Some(_old) = name.replace(label) {
-                                panic!("Multiple labels can't be return for address")
-                            }
-                        }
-                        idb_rs::id0::AddressInfo::TilType(addr_ty) => {
-                            ensure!(
-                                matches!(&addr_ty, idb_rs::til::Type::Function(_)),
-                                "Type for function at {fun_addr:#?} is invalid"
-                            );
-                            if let Some(_old) = ty.replace(addr_ty) {
-                                panic!("Multiple types can't be return for address")
-                            }
-                        }
-                    }
-                }
-                match (name, ty) {
-                    (Some(name), Some(ty)) => println!("\"{name}\":{ty:?}"),
-                    (None, None) => println!("NO_INFO"),
-                    (None, Some(ty)) => println!("UNAMED:{ty:?}"),
-                    (Some(name), None) => println!("\"{name}\""),
-                }
-            }
+            DirTreeEntry::Leaf(fun_addr) => print_function(id0, *fun_addr)?,
             DirTreeEntry::Directory { name, entries } => {
                 println!("{name}:");
                 print_folder(id0, identation + 1, entries)?;
@@ -57,7 +27,39 @@ fn print_folder(id0: &ID0Section, identation: usize, dirs: &[DirTreeEntry<u64>])
     Ok(())
 }
 
-fn print_ident(ident: usize) {
-    let data = vec![b' '; ident];
+pub fn print_function(id0: &ID0Section, address: u64) -> Result<()> {
+    let infos = id0.address_info_at(address)?;
+    let mut name = None;
+    let mut ty = None;
+    for info in infos {
+        match info? {
+            idb_rs::id0::AddressInfo::Comment(_) | idb_rs::id0::AddressInfo::Other { .. } => {}
+            idb_rs::id0::AddressInfo::Label(label) => {
+                if let Some(_old) = name.replace(label) {
+                    panic!("Multiple labels can't be return for address")
+                }
+            }
+            idb_rs::id0::AddressInfo::TilType(addr_ty) => {
+                ensure!(
+                    matches!(&addr_ty, idb_rs::til::Type::Function(_)),
+                    "Type for function at {address:#?} is invalid"
+                );
+                if let Some(_old) = ty.replace(addr_ty) {
+                    panic!("Multiple types can't be return for address")
+                }
+            }
+        }
+    }
+    match (name, ty) {
+        (Some(name), Some(ty)) => println!("\"{name}\":{ty:?}"),
+        (None, None) => println!("NO_INFO"),
+        (None, Some(ty)) => println!("UNAMED:{ty:?}"),
+        (Some(name), None) => println!("\"{name}\""),
+    }
+    Ok(())
+}
+
+fn print_indent(indent: usize) {
+    let data = vec![b' '; indent];
     print!("{}", String::from_utf8(data).unwrap());
 }
