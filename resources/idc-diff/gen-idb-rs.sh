bf3d07de4c3eac4c69f525c9790914b9561cc3f8
rbran <git@rubens.io>
2025-07-24 09:07:05 -0300

    add ida idc generation script

--- resources/idc-diff/gen-idb-rs.sh
+++ resources/idc-diff/gen-idb-rs.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-find ../idbs/ -name '*.i64' | while read line
+sh -c "find ../idbs/ -name '*.i64'; find ../idbs/ -name '*.idb'" | while read line
 do
  file=$(echo $line | cut -d '/' -f 3-)
  mkdir -p "idb-rs/$(dirname ${file})"
- cargo run --all-features --manifest-path ${HOME}/src/idb-rs/Cargo.toml --release --bin idb-tools -- --input ${line} produce-idc > "idb-rs/${file}.idc"
+ cargo run --all-features --manifest-path ${HOME}/src/idb-rs/Cargo.toml --release --bin idb-tools -- --input ${line} produce-idc > "idb-rs/${file}.idc" "$@"
 done
