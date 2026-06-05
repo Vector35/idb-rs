67503f0bb06b1d38adfc43328e01b641ba0bdc24
Chris Kader <ChrisKader@users.noreply.github.com>
2026-06-05 16:45:08 -0500

    tools: report enum-displayed operands in dump-address-info
    Print op_enum tid, the resolved member name and the owning enumeration
    (op_enum_type) for operands 0 and 1, so the address-info dump can be used to
    inspect which operands IDA displays against an enumeration.

--- src/tools/dump_addr_info.rs
+++ src/tools/dump_addr_info.rs
@@ -1,4 +1,4 @@
-use crate::{get_id0_id1_id2_sections, Args, Id0Id1Id2Variant};
+use crate::{get_id0_id1_id2_sections, get_til_section, Args, Id0Id1Id2Variant};
 
 use anyhow::Result;
 
@@ -6,14 +6,18 @@ use idb_rs::addr_info::all_address_info;
 use idb_rs::{IDAKind, IDAVariants};
 
 pub fn dump_addr_info(args: &Args) -> Result<()> {
+    let til = get_til_section(args).ok();
     // parse the id0 sector/file
     match get_id0_id1_id2_sections(args)? {
-        IDAVariants::IDA32(kind) => dump_inner(kind),
-        IDAVariants::IDA64(kind) => dump_inner(kind),
+        IDAVariants::IDA32(kind) => dump_inner(kind, til.as_ref()),
+        IDAVariants::IDA64(kind) => dump_inner(kind, til.as_ref()),
     }
 }
 
-fn dump_inner<K: IDAKind>((id0, id1, id2): Id0Id1Id2Variant<K>) -> Result<()> {
+fn dump_inner<K: IDAKind>(
+    (id0, id1, id2): Id0Id1Id2Variant<K>,
+    til: Option<&idb_rs::til::section::TILSection>,
+) -> Result<()> {
     // TODO create a function for that in ida_info
     let root_info_idx = id0.root_node()?;
     let root_info = id0.ida_info(root_info_idx)?;
@@ -46,6 +50,18 @@ fn dump_inner<K: IDAKind>((id0, id1, id2): Id0Id1Id2Variant<K>) -> Result<()> {
         }
         if let Some(tinfo) = addr_info.tinfo(&root_info)? {
             write!(&mut buf, " Tinfo: {tinfo:?}",)?;
+        }
+        for operand in 0u8..2 {
+            if let Some(id) = addr_info.op_enum(operand) {
+                let name = addr_info.op_enum_name(operand);
+                let enum_ty = til.and_then(|t| addr_info.op_enum_type(operand, t));
+                write!(
+                    &mut buf,
+                    " OpEnum[{operand}]: tid={id:#x} member={:?} enum_type={:?}",
+                    name.as_ref().map(|n| n.as_utf8_lossy()),
+                    enum_ty.map(|t| t.name.as_utf8_lossy())
+                )?;
+            }
         }
         if !buf.is_empty() {
             println!("{:#010x?}:{buf}", addr.into_raw());
